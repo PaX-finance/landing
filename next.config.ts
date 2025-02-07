fb06f5e1221e804c0bb21da7f10cedf9277ddcf1
OxMarco <>
2025-02-07 20:09:05 +0100

    remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/landing',
-  assetPrefix: '/landing/',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
