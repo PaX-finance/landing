492dfd0d66104137d97e393e6892c045926a1b06
OxMarco <>
2025-02-07 19:57:23 +0100

    basePath

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NEXT_PUBLIC_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? '/landing' : '',
-  assetPrefix: isProd ? '/landing/' : '',
+  basePath: '/landing',
+  assetPrefix: '/landing/',
   images: {
     unoptimized: true,
   },
